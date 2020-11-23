25858fdde50504068e7914365deb5e7e5d459358
sreejitha-mc <sreejitha.mc19@gmail.com>
2020-11-23 12:53:32 +0530

    removed commented function

--- src/app/services/logins.service.ts
+++ src/app/services/logins.service.ts
@@ -16,19 +16,4 @@ export class LoginsService {
         return this.http.get<User[]>(this.loginUrl);
     }
 
-    /* validateLogins(): Observable<boolean> {
-        this.getLogin().subscribe(
-            (data) => {
-                const username = 'admin';
-                const pass = 'admin';
-                const user = data.find(u => u.username === username && u.password === pass);
-                return user ? of(true) : of(false);
-            },
-            (err) => {
-                console.log(err);
-                return of(false);
-            }
-        );
-    }
- */
 }
